600d4c5176578b5937c873e1e192a9abcab94b1a
OgagaAkpowenre1 <136248640+OgagaAkpowenre1@users.noreply.github.com>
2024-06-02 11:38:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import FeatureFlags from './components/feature-flags';
-import FeatureFlagsGlobalState from './components/feature-flags/context/featureFlag';
 import Weather from './weather-app/weather';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <FeatureFlagsGlobalState>
-        <FeatureFlags />
-      </FeatureFlagsGlobalState> */}
       <Weather />
     </div>
   );
